0aa723f843c5753137d2d9b366a463009cf1ecf7
CrazyHuman00 <asakurashi00@icloud.com>
2024-06-24 23:03:45 +0900

    first commit

---
+++ shebang.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+: << USAGE_CREATE_SHEBANG
+------------------------
+# Change current directory to top of AP Lecture
+$ pwd
+/Users/xxx/AP/Java
+
+# Specify a repository name at the first argument.
+$ checkaoki HelloWorld
+------------------------
+USAGE_CREATE_SHEBANG
+echo "aaa"
